801499538835e3bbf7f2d24470697ee162ec38e7
Sachin <skatyal@seas.upenn.edu>
2020-08-04 19:42:44 -0500

    connect playlist with api

--- src/components/Playlists/Playlists.js
+++ src/components/Playlists/Playlists.js
@@ -8,6 +8,10 @@ class Playlists extends Component {
     constructor(props) {
         super(props)
 
+        this.state = {
+            playlists: []
+        }
+
         if (this.props.token != null) {
             spotifyWebApi.setAccessToken(this.props.token);
         }
@@ -16,6 +20,23 @@ class Playlists extends Component {
 
     async componentDidMount() {
         //handle getting all the playlist information
+        const playlists = []
+
+        const temp = await spotifyWebApi.getUserPlaylists()
+        for (var playlist of temp.items) {
+            try {
+                playlists.push({
+                    image:playlist.images[0].url,
+                    name:playlist.name,
+                    id: playlist.id
+                })
+            } catch{}
+        }
+        console.log(playlists)
+
+        this.setState({
+            playlists
+        })
     }
 
     render() {
@@ -24,20 +45,11 @@ class Playlists extends Component {
               <div class="container-fluid py-2">
                 <h3 class="font-weight-light">Playlists</h3>
                 <div class="d-flex flex-row flext-nowrap overflow-auto">
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
-                    <div class="card card-body">Card</div>
+                    {this.state.playlists.map(playlist => 
+                        <div key = {playlist.id} class = "card card-body">
+                            <img src = {playlist.image}/>
+                        </div>
+                    )}
                 </div>
             </div>  
             </>
